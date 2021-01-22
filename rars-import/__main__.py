574d452297bf9a4153caa63e37de3ec79b06ff1b
Georgi Yolovski <georgi.yolovski@accedia.com>
2021-01-22 17:25:40 +0200

    Updated the utility for daily import

--- rars-import/__main__.py
+++ rars-import/__main__.py
@@ -5,7 +5,7 @@ import MySQLdb.cursors as cursors
 from datetime import datetime
 from google.cloud import storage
 from google.cloud import bigquery
-from datetime import datetime
+from datetime import datetime, timedelta
 import threading
 
 def flush_to_file(table, data, header, storage_client, bq_client, google_folder, bucket_name, bq_dataset):
@@ -43,6 +43,14 @@ def upload_and_load_file(table, storage_client, bq_client, google_folder, bucket
   print(f'Loading {uri} into {table_id}')
   load_job = bq_client.load_table_from_uri(uri, table_id, job_config=job_config)
 
+def get_period_column(columns_result):
+    date_columns = list(filter(lambda x: x[1] == 'date', columns_result))
+    datetime_columns = list(filter(lambda x: x[1] == 'datetime', columns_result))
+    period_column = date_columns if len(date_columns) > 0 else datetime_columns
+    valid_period_column = period_column if len(period_column) == 1 else []
+    filtered_period_column = list(filter(lambda x: x[0] != 'last_modified', valid_period_column))
+    return filtered_period_column[0] if len(filtered_period_column) == 1 else None
+
 def main():
     mydb = MySQLdb.connect(
         host = os.environ['MYSQL_HOST'],
@@ -61,7 +69,9 @@ def main():
     tables = list(map(lambda x: x[0], tables_cursor))
     print(f'Tables: {tables}')
 
-    google_folder = os.environ['GCS_FOLDER']
+
+    yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    google_folder = f'{os.environ["GCS_FOLDER"]}/{yesterday}'
     bucket_name = os.environ['GCS_BUCKET']
     bq_dataset = os.environ['BQ_DATASET']
 
@@ -69,11 +79,23 @@ def main():
     for table in tables:
         header_cursor = mydb.cursor()
         header_cursor.execute(f'SHOW COLUMNS FROM `{schema}`.{table}')
-        header_result = header_cursor.fetchall()
-        header = list(map(lambda x: x[0].replace('.', '_').replace('-', '_').replace('+', '_plus'), header_result))
+        columns_result = header_cursor.fetchall()
+        header = list(map(lambda x: x[0].replace('.', '_').replace('-', '_').replace('+', '_plus'), columns_result))
+        period_column = get_period_column(columns_result)
 
-        print(f'Retrieving data for {table}')
-        mycursor.execute(f'SELECT * FROM  `{schema}`.{table}')
+        query = f'SELECT * FROM  `{schema}`.{table}'
+        if period_column is not None:
+          if period_column[1] == 'date':
+            query = query + f' WHERE {period_column[0]} = \'{yesterday}\''
+          else:
+            query = query + f' WHERE CAST({period_column[0]} AS DATE) = \'{yesterday}\''
+        else:
+          truncate_query = f'DELETE FROM `{bq_dataset}`.{table} WHERE 1=1'
+          query_job = bq_client.query(truncate_query)
+          result = query_job.result()
+          print(f'Truncated table `{bq_dataset}`.{table}: {result}')
+        print(f'Retrieving data for {table}: {query}')
+        mycursor.execute(query)
 
         rows = []
         for row in mycursor:
